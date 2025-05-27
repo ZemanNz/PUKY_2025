7de20ff4e1a7586e7071803561e0c602b342477c
Nik <nikzeman37@gmail.com>
2025-05-27 19:05:55 +0200

    motor commands zkali

--- include/motor_commands.h
+++ include/motor_commands.h
@@ -229,6 +229,9 @@ void radius_l(int degrees, int polomer, int speed){
             right_done = true;
         });
     }
+    while(!left_done || !right_done) {
+        delay(10); // čekáme na dokončení obou motorů
+    }
 }
 void turn_on_spot(int degrees){
   rkMotorsDrive(((degrees/360.0) * PI * roztec) * koeficient_turn , ((degrees/360.0) * -PI * roztec)* koeficient_turn, 30 , 30 );
@@ -253,7 +256,7 @@ void back_buttons(int speed)
         });
 
         odchylka = M1_pos - M4_pos;
-        integral += odchylka; 
+        integral += odchylka;
 
         man.motor(rb::MotorId::M1).power(speed * 320);
         man.motor(rb::MotorId::M4).power(-speed * 320 + odchylka * Kp + integral * Ki + (odchylka - last_odchylka) * Kd);
