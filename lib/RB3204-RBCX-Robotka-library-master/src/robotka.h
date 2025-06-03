adc2b654ef2e037642f811a04808fd5bb40a3534
Nik <nikzeman37@gmail.com>
2025-06-03 08:25:20 +0200

    První commit

--- lib/RB3204-RBCX-Robotka-library-master/src/robotka.h
+++ lib/RB3204-RBCX-Robotka-library-master/src/robotka.h
@@ -63,8 +63,8 @@ struct rkConfig {
         , motor_polarity_switch_right(true)
         , motor_enable_failsafe(false)
         , motor_wheel_diameter(67)
-        , motor_max_ticks_per_second(5800)
-        , motor_max_acceleration(18000)
+        , motor_max_ticks_per_second(2600)
+        , motor_max_acceleration(50000)
         , stupid_servo_min(-1.65f)
         , stupid_servo_max(1.65f) {
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,61 @@
 #include <Arduino.h>
-#include <stdint.h>
 #include "robotka.h"
 #include "smart_servo_command.h"
 #include "motor_commands.h"
 
-#define RX1_PIN   16
-#define TX1_PIN   17  // mimochodem nepotřebujeme, ale Arduino vyžaduje parametry
-
-// Stejná struktura (packed implicitně)
-typedef struct __attribute__((packed)) {
-    uint8_t  id;
-    int16_t  value;
-} ServoMsg;
-
-
-const uint8_t SYNC0 = 0xAA;
-const uint8_t SYNC1 = 0x55;
-
-enum RxState { WAIT_SYNC0, WAIT_SYNC1, READ_PAYLOAD };
-RxState state = WAIT_SYNC0;
-
-static const size_t PAYLOAD_SIZE = sizeof(ServoMsg);
-uint8_t buf[PAYLOAD_SIZE];
-size_t idx = 0;
+using namespace lx16a; // aby nebylo třeba to psát všude
 
 void setup() {
-  rkConfig cfg;
-  rkSetup(cfg);
-  
-  rkLedRed(true); // Turn on red LED
-  rkLedBlue(true); // Turn on blue LED
-  printf("Robotka started!\n");
-  delay(5000); // Wait for 1 second
-  forward(2000, 70); // Call a motor function
-  delay(500); // Wait for the motor function to execute
-  Serial1.begin(115200, SERIAL_8N1, RX1_PIN, TX1_PIN);
-  auto &bus = rkSmartServoBus(2);
-  s_s_init(bus, 1, 0, 239);
-  s_s_init(bus, 0, 70, 220);
-  printf("Servo 0 je na pozici %f stupnu\n", bus.pos(0).deg());
-  printf("Servo 1 je na pozici %f stupnu\n", bus.pos(1).deg());
-  printf("done\n");
-  while (true) {
-    while (Serial1.available()) {
-    uint8_t c = Serial1.read();
-    printf("c: %02X\n", c);
-    switch (state) {
-      case WAIT_SYNC0:
-        if (c == SYNC0) state = WAIT_SYNC1;
-        break;
-      case WAIT_SYNC1:
-        if (c == SYNC1) {
-          state = READ_PAYLOAD;
-          idx = 0;
-        } else {
-          // možná c je už SYNC0, zkusit znovu
-          state = (c == SYNC0) ? WAIT_SYNC1 : WAIT_SYNC0;
-        }
-        break;
-      case READ_PAYLOAD:
-        buf[idx++] = c;
-        if (idx >= PAYLOAD_SIZE) {
-          // máme celý payload
-          ServoMsg m;
-          memcpy(&m, buf, PAYLOAD_SIZE);
-          printf("Received: ");
-          printf("ID: %d, Value: %d\n", m.id, m.value);
-          // zpracování zprávy
-          s_s_move(bus, m.id, m.value, 50.0);
-          // zpracování zprávy
-          state = WAIT_SYNC0;
-        }
-        break;
-    }
-  }
-  }
-}
+    rkConfig cfg;
+    rkSetup(cfg);
+
+    printf("Robotka started!\n");
+    pinMode(Bbutton1, INPUT_PULLUP);
+    pinMode(Bbutton2, INPUT_PULLUP);
+    // Pripoj jedni chytre servo s id 0
+    //auto &bus = rkSmartServoBus(1);
+    auto &bus = rkSmartServoBus(2);
+    s_s_init(bus, 1, 50, 190);
+    s_s_init(bus, 0, 10, 110);
+    rkLedRed(true); // Turn on red LED
+    rkLedBlue(true); // Turn on blue LED
+
+}//230- dole 0 -nahore
 
 void loop() {
-  
+  auto &bus = rkSmartServoBus(2);
+  //auto &bus = rkSmartServoBus(1);
+  if (rkButtonIsPressed(BTN_UP)) {
+      s_s_move(bus, 0, 100, 80.0); // nahoru
+      delay(5000);
+      s_s_move(bus, 0, 10, 80.0); // nahoru
+      delay(5000);
+      rkServosSetPosition(1, 90); // Servo 1 nastaví na 90°
+      delay(5000);
+      rkServosSetPosition(1, -90); // Servo 1 nastaví na 0°
+  }
+  if (rkButtonIsPressed(BTN_DOWN)) {
+      s_s_move(bus, 1, 190, 80.0); // dolu
+      delay(5000);
+      s_s_move(bus, 1, 120, 80.0); // dolu
+      delay(5000);
+      rkServosSetPosition(1, 90); // Servo 1 nastaví na 90°
+      delay(5000);
+      rkServosSetPosition(1, 0); // Servo 1 nastaví na 0°
+  }
+  if (rkButtonIsPressed(BTN_ON)) {//nahoru -------------zavirame 0------------ otevreny na 160
+    forward(1000, 70);
+  }
+  if (rkButtonIsPressed(BTN_OFF)) {//dolu
+    back_buttons(40);
+  }
+  if ((digitalRead(Bbutton1) == LOW)){
+            rkLedYellow(true); // Turn on red LED
+            rkLedGreen(false); // Turn on red LED
+        }
+  if ((digitalRead(Bbutton2) == LOW)){
+            rkLedGreen(true); // Turn on red LED
+            rkLedYellow(false); // Turn on red LED
+        }
+  delay(50);
 }
